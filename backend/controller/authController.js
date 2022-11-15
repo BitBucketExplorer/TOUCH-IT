8adeeb3ba896201a2b498c42c00e9e59255de9cc
Vikky <krishnayadav369992022@gmail.com>
2022-11-15 22:47:28 +0530

    commit uploading

--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -6,7 +6,7 @@ const sendEmail = require('../utils/sendEmail');
 const crypto = require('crypto');
 
 /**
- * Register a user /api/v1/register
+ * Register a user  => /api/v1/register
  */
 exports.registerUser = catchAsyncErrors(async (req, res, next) => {
 
@@ -22,6 +22,7 @@ exports.registerUser = catchAsyncErrors(async (req, res, next) => {
     });
     sendTokenToken(user, 200, res);
 });
+
 /**
  * Login user =>/api/v1/login
  */
@@ -46,8 +47,9 @@ exports.loginUser = catchAsyncErrors(async (req, res, next) => {
     }
     sendToken(user, 200, res);
 });
+
 /**
- * Forgot Password /api/v1/password/forgot
+ * Forgot Password   => /api/v1/password/forgot
  */
 exports.forgotPassword = catchAsyncErrors(async (req, res, next) => {
     const user = await User.findOne({ email: req.body.email });
@@ -77,8 +79,9 @@ exports.forgotPassword = catchAsyncErrors(async (req, res, next) => {
         return next(new ErrorHandler('error.message', 500))
     }
 });
+
 /**
- * Reset Password /api/v1/password/forgot
+ * Reset Password  => /api/v1/password/forgot
  */
 exports.resetPassword = catchAsyncErrors(async (req, res, next) => {
     //Hash URL token
@@ -100,9 +103,57 @@ exports.resetPassword = catchAsyncErrors(async (req, res, next) => {
 
     await user.save();
     sendToken(user, 200, res);
-})
+});
+
 /**
- * Logout user /api/v1/logout
+ * Get currently logged in user detail => /api/v1/me
+ */
+exports.getUserProfile = catchAsyncErrors(async (req, res, next) => {
+    const user = await User.findById(req.user.id);
+    res.status(200).json({
+        success: true,
+        user
+    })
+});
+
+/**
+ * Change/Update current looged user password  =>  /api/vi/password/update
+ */
+exports.updatePassword = catchAsyncErrors(async (req, res, next) => {
+    const user = await User.findById(req.user.id).select('+password');
+    //Check previous user password
+    const isMatched = await user.comparePassword(req.body.oldPassword);
+    if (!isMatched) {
+        return next(new ErrorHandler('Old password is incorrect', 400));
+    }
+    user.password = req.body.password;
+    await user.save();
+    sendToken(user, 200, res);
+});
+
+/**
+ * Update user profile  => /api/v1/me/update
+ */
+exports.updateProfile = catchAsyncErrors(async (req, res, next) => {
+    const newUserDate = {
+        name: req.body.name,
+        email: req.body.email
+    }
+    //Update Avatar: TODO
+    const user = await User.findByIdAndUpdate(req.user.id, newUserDate, {
+        new: true,
+        runValidators: true,
+        useFindAndModify: false
+    });
+
+    res.status(200).json({
+        success: true,
+    })
+
+})
+
+/**
+ * Logout user  => /api/v1/logout
  */
 exports.logout = catchAsyncErrors(async (req, res, next) => {
     res.cookie('token', null, {
@@ -114,3 +165,69 @@ exports.logout = catchAsyncErrors(async (req, res, next) => {
         message: 'Logged out'
     })
 });
+
+/**
+ * Admin Routes- Get All Users  =>  /api/v1/admin/users
+ */
+exports.allUsers = catchAsyncErrors(async (req, res, next) => {
+    const users = await User.find();
+    res.status(200).json({
+        success: true,
+        users
+    })
+});
+
+/**
+ * Admin Routes- Get User Details  =>  /api/v1/admin/user/:id
+ */
+exports.getUserDetails = catchAsyncErrors(async (req, res, next) => {
+    const user = await User.findById(req.params.id);
+    if (!user) {
+        return next(new ErrorHandler(`User does not found with id : ${req.params.id}`, 400))
+    }
+    res.status(200).json({
+        success: true,
+        user
+    })
+});
+
+/**
+ * Admin Routes- Update user profile  => /api/v1/admin/user/:id
+ */
+exports.updateUser = catchAsyncErrors(async (req, res, next) => {
+    const newUserDate = {
+        name: req.body.name,
+        email: req.body.email,
+        role: req.body.role,
+    }
+    //Update Avatar: TODO
+    const user = await User.findByIdAndUpdate(req.params.id, newUserDate, {
+        new: true,
+        runValidators: true,
+        useFindAndModify: false
+    });
+
+    res.status(200).json({
+        success: true,
+    })
+
+});
+
+/**
+ * Admin Routes- Delete user  => /api/v1/admin/user/:id
+ */
+exports.deleteUser = catchAsyncErrors(async (req, res, next) => {
+    const user = await User.findById(req.params.id);
+    if (!user) {
+        return next(new ErrorHandler(`User not found for id : ${req.params.id}`));
+    }
+
+    //Remove Avatar from cloudnary TODO
+
+    await user.remove();
+
+    res.status(200).json({
+        success: true,
+        message: 'User deleted !'
+    })
+})
--- backend/middlewares/errors.js
+++ backend/middlewares/errors.js
@@ -28,7 +28,21 @@ module.exports = (err, req, res, next) => {
             const message = Object.values(err.errors).map(value => value.message);
             error = new ErrorHandler(message, 400);
         }
-
+        //Handle Mongoose duplicate key error
+        if (err.code === 11000) {
+            const message = `Duplicate ${Object.keys(err.keyValue)} entered`
+            error = new ErrorHandler(message, 400);
+        }
+        //Hadle Wrong JWT error
+        if (err.name === "JsonWebTokenError") {
+            const message = 'JSON web token is invalid. Try Again !!!'
+            error = new ErrorHandler(message, 400);
+        }
+        //Hadle Expired JWT error
+        if (err.name === "TokenExpiredError") {
+            const message = 'JSON web token is expired. Try Again !!!'
+            error = new ErrorHandler(message, 400);
+        }
         res.status(error.statusCode).json({
             success: false,
             message: error.message || 'Internal Server Error'
